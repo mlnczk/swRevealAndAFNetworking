a231c20d94a0c9d91c90568095e197cea500ce68
Rafal Kampa <rafalkampa@MacBook-Air-Rafal.local>
2016-05-17 11:02:34 +0200

    cocoaPods

---
+++ afnetworking/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  afnetworking
+//
+//  Created by Rafal Kampa on 17.05.2016.
+//  Copyright © 2016 Rafal Kampa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ afnetworking/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  afnetworking
+//
+//  Created by Rafal Kampa on 17.05.2016.
+//  Copyright © 2016 Rafal Kampa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
